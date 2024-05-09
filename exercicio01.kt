17e7639eb8f0db90f088e87e698b7c91b31633e9
Amanda093 <vovigadagamer@gmail.com>
2024-05-09 08:37:05 -0300

    update

--- exercicio01.kt
+++ exercicio01.kt
@@ -1,14 +1,14 @@
+fun main() {
+    val event1 = Event("Study Kotlin", "Commit to studying Kotlin at least 15 minutes per day.", "Evening", 15)
+    println(event1.toString())
+}
+
 data class Event(
     val title: String,
     val description: String? = null,
     val daypart: String,
     val duration: Int,
 )
-
-fun main() {
-    val event1 = Event("Study Kotlin", "Commit to studying Kotlin at least 15 minutes per day.", "Evening", 15)
-    println(event1.toString())
-}
 
 /* 
 
